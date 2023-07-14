c302514fa965334907e5141b1f8673ef04ab91de
hogi-wan-kenobi <102341411+hogi-wan-kenobi@users.noreply.github.com>
2023-07-14 16:48:04 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-//$(document).ready(function() {
-//  $('#contact-form').submit(function(event) {
-//    event.preventDefault(); // Prevent form submission
-//
-//    var form = $(this);
-//    var apiKey = 'patDUTTQKz5M7Dvnw.e41fdf3c988623b4706c2bf28e98e0d6172d8cd825030cdae9866d68b7584553'; // Replace with your Airtable API key
-//    var baseId = 'appvjU455Si4NmOvp'; // Replace with your Airtable base ID
-//    var tableName = 'Contact%20Form%20Entries'; // Replace with your Airtable table name
-//    var url = 'https://api.airtable.com/v0/' + baseId + '/' + tableName + '?api_key=' + apiKey;
-//    var formData = form.serialize();
-//
-//    $.ajax({
-//      type: 'POST',
-//      url: url,
-//      dataType: 'json',
-//      data: formData,
-//      success: function(response) {
-//        form[0].reset();
-//        alert('Message sent successfully!');
-//      },
-//      error: function(xhr, status, error) {
-//        alert('Failed to send message. Please try again later.');
-//      }
-//    });
-//  });
-//});
+$(document).ready(function() {
+  $('#contact-form').submit(function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var form = $(this);
+    var apiKey = 'patDUTTQKz5M7Dvnw.e41fdf3c988623b4706c2bf28e98e0d6172d8cd825030cdae9866d68b7584553'; // Replace with your Airtable API key
+    var baseId = 'appvjU455Si4NmOvp'; // Replace with your Airtable base ID
+    var tableName = 'Contact%20Form%20Entries'; // Replace with your Airtable table name
+    var url = 'https://api.airtable.com/v0/' + baseId + '/' + tableName + '?api_key=' + apiKey;
+    var formData = form.serialize();
+
+    $.ajax({
+      type: 'POST',
+      url: url,
+      dataType: 'json',
+      data: formData,
+      success: function(response) {
+        form[0].reset();
+        alert('Message sent successfully!');
+      },
+      error: function(xhr, status, error) {
+        alert('Failed to send message. Please try again later.');
+      }
+    });
+  });
+});
 
 //USE PHP TO HANDLE AIRTABLE API
 
