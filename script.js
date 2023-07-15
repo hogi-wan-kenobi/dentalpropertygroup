3c07bdf9e348f64ff6522ba12f254c65e7e8fca4
hogi-wan-kenobi <102341411+hogi-wan-kenobi@users.noreply.github.com>
2023-07-15 00:23:39 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,7 @@
-$(document).ready(function() {
-  $('#contact-form').submit(function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    var form = $(this);
-    var apiKey = 'patDUTTQKz5M7Dvnw.e41fdf3c988623b4706c2bf28e98e0d6172d8cd825030cdae9866d68b7584553'; // Replace with your Airtable API key
-    var baseId = 'appvjU455Si4NmOvp'; // Replace with your Airtable base ID
-    var tableName = 'Contact%20Form%20Entries'; // Replace with your Airtable table name
-    var url = 'https://api.airtable.com/v0/' + baseId + '/' + tableName + '?api_key=' + apiKey;
-    var formData = form.serialize();
-
-    $.ajax({
-      type: 'POST',
-      url: url,
-      dataType: 'json',
-      data: formData,
-      success: function(response) {
-        form[0].reset();
-        alert('Message sent successfully!');
-      },
-      error: function(xhr, status, error) {
-        alert('Failed to send message. Please try again later.');
-      }
-    });
-  });
-});
-
-//USE PHP TO HANDLE AIRTABLE API
-
-//$(document).ready(function() {
-//  $('#contact-form').submit(function(event) {
-//    event.preventDefault(); // Prevent form submission
-//
-//    var form = $(this);
-//    var formData = form.serialize();
-//
-//    $.ajax({
-//      type: 'POST',
-//      url: 'submit.php', // Path to your PHP script
-//      data: formData,
-//      success: function(response) {
-//        form[0].reset();
-//        alert('Message sent successfully!');
-//      },
-//      error: function(xhr, status, error) {
-//        alert('Failed to send message. Please try again later.');
-//      }
-//    });
-//  });
-//});
-
 var expandButtons = document.querySelectorAll('.expand-btn');
 var overlays = document.querySelectorAll('.popup');
 var closeButtons = document.querySelectorAll('.close-btn');
+var menuButtons = document.querySelectorAll('.menu-btn');
 
 expandButtons.forEach(function(button) {
   button.addEventListener('click', function() {
@@ -71,3 +21,13 @@ closeButtons.forEach(function(button) {
     popup.style.display = 'none';
   });
 });
+
+menuButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var overlay = document.getElementById('overlay');
+    var popup = document.getElementById('popup1');
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
+  });
+});
+
